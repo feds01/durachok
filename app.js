f05020913dcf698ab09b46b7ab125d785b9045ef
Alex <alexander.fedotov.uk@gmail.com>
2020-09-11 12:02:02 +0100

    Created simple websocket server implementation

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+import express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//initialize a simple http server
+const server = http.createServer(app);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+wss.on('connection', (ws) => {
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediately a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+//start our server
+server.listen(process.env.PORT || 5000, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
 });
-
-module.exports = app;
