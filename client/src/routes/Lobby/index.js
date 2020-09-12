bed5e2cf6100aa8d5083460a8b384af341ab4038
Alex <alexander.fedotov.uk@gmail.com>
2020-09-12 12:53:55 +0100

    fix issue with booting react app and mismatched packages between client and server

---
+++ client/src/routes/Lobby/index.js
@@ -0,0 +1,19 @@
+/**
+ * Module description:   src/routes/Lobby/index.js
+ *
+ * Created on 11/09/2020
+ * @author Alexander. E. Fedotov
+ * @email <alexander.fedotov.uk@gmail.com>
+ */
+
+import React from "react";
+
+const LobbyRoute = () => {
+    return (
+        <React.Fragment>
+            Lobby
+        </React.Fragment>
+    );
+};
+
+export default LobbyRoute;
---
+++ client/src/routes/Login/index.js
@@ -0,0 +1,19 @@
+/**
+ * Module description:   src/routes/Home/index.js
+ *
+ * Created on 11/09/2020
+ * @author Alexander. E. Fedotov
+ * @email <alexander.fedotov.uk@gmail.com>
+ */
+
+import React from "react";
+
+const LoginRoute = () => {
+    return (
+        <React.Fragment>
+            Login
+        </React.Fragment>
+    );
+};
+
+export default LoginRoute;
