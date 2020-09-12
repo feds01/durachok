6604706ccacc55b1ad48984a148df8af1c948302
Alex <alexander.fedotov.uk@gmail.com>
2020-09-12 12:53:55 +0100

    fix issue with booting react app and mismatched packages between client and server

---
+++ client/src/routes/Home/index.js
@@ -0,0 +1,19 @@
+/**
+ * Module description:   src/routes/Home/index.js
+ *
+ * Created on 11/09/2020
+ * @author Alexander. E. Fedotov
+ * @email <alexander.fedotov.uk@gmail.com>
+ */
+
+import React from "react";
+
+const HomeRoute = () => {
+    return (
+        <React.Fragment>
+            Home
+        </React.Fragment>
+    );
+};
+
+export default HomeRoute;
