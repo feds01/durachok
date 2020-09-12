c19fc5a112ee1a5aed0e42b6f4857d501b4614a1
Alex <alexander.fedotov.uk@gmail.com>
2020-09-12 22:56:29 +0100

    Added GamePin input and GameSecurity input for validating game lobby pin and lobby security pin for two-factor authentication.

--- client/src/components/Prompt/index.js
+++ client/src/components/Prompt/index.js
@@ -1,85 +1,43 @@
 import './index.scss';
 import React from 'react';
-import {Formik} from "formik";
-import Loader from 'react-loader-spinner';
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import GamePin from "./GamePin";
+import GameSecurity from "./GameSecurity";
+import {CSSTransition} from 'react-transition-group';
 
-const Prompt = () => {
-    return (
-        <Formik
-            initialValues={{pin: ''}}
-            validateOnChange={false}
-            validate={(values) => {
-                const errors = {};
+class Prompt extends React.Component {
+    constructor(props) {
+        super(props);
 
-                if (!values.pin.match(/^[0-9]*$/gm)) errors.pin = 'Game PIN should be numerical.';
-                else if (values.pin.length !== 6) errors.pin = 'Game PIN is 6 digits long.';
+        this.state = {
+            stage: 'pin',
+            pin: undefined,
+        }
+    }
 
-                return errors;
-            }}
-            onSubmit={async (values, {setSubmitting, setErrors}) => {
-                // await new Promise(res => setTimeout(res, 500));
+    render() {
+        const {stage, pin} = this.state;
 
-                // make a request to the API to check if there is a game with the given pin,
-                // and if so we'll set the next stage of the prompt (enter the pin).
-                if (values.pin === '123456') console.log('yay!')
-                else setErrors({pin: "Invalid game PIN."})
-
-                setSubmitting(false);
-            }}
-        >
-            {props => {
-                const {
-                    values,
-                    errors,
-                    isSubmitting,
-                    handleSubmit,
-                    handleChange
-                } = props;
-
-                return (
-                    <div className={'Prompt'}>
-                        <TextField
-                            id={'pin'}
-                            className={'Prompt-code'}
-                            placeholder={'Enter game PIN'}
-                            autoFocus
-                            error={Boolean(errors.pin)}
-                            helperText={errors.pin || ""}
-                            inputProps={{
-                                style:
-                                    {
-                                        textAlign: 'center',
-                                        color: '#dad8ec',
-                                        width: 360,
-                                        fontSize: 20,
-                                        letterSpacing: 2
-                                    }
-                            }}
-                            variant={'filled'}
-                            value={values.pin}
-                            onChange={handleChange}
-                        />
-                        <Button
-                            variant={'contained'}
-                            className={'Prompt-enter'}
-                            disableElevation
-                            style={{
-                                marginTop: 19
-                            }}
-                            disableRipple
-                            disabled={isSubmitting}
-                            onClick={handleSubmit}
-                            color={'primary'}
-                        >
-                            {isSubmitting ? <Loader type="ThreeDots" color="#FFFFFF" height={20} width={40} /> : "Enter"}
-                        </Button>
-                    </div>
-                );
-            }}
-        </Formik>
-    );
-};
+        // Essentially we first render the game pin if the stage is equal to 'pin'. If the 'pin' stage
+        // returns a query to progress to the next stage, then we push it to the next stage of 'security'.
+        // The next stage requires the user to enter a admin provided game code to finalise the entry into
+        // the lobby.
+        return (
+            <div>
+                {stage === 'pin' && <GamePin onSuccess={(pin) => {
+                    this.setState({pin: pin, stage: 'security'})
+                }}/>}
+                <CSSTransition
+                    in={stage === 'security'}
+                    timeout={300}
+                    appear
+                    classNames={'security'}
+                    unmountOnExit
+                >
+                    <GameSecurity pin={pin}/>
+                </CSSTransition>
+            </div>
+        );
+    }
+}
 
 export default Prompt;
