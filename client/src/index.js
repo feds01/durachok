cacdcefd55c1488739cd83fbb08357933e06e856
Alex <alexander.fedotov.uk@gmail.com>
2020-09-12 20:59:01 +0100

    Added loader when a game pin is being submitted.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
+import './index.css';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
