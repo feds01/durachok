2f1c8ac8bf953afff300427f1b7941fb6d4bee6a
Alex <alexander.fedotov.uk@gmail.com>
2020-10-07 19:29:52 +0100

    Added documentation for lobby routes.

--- api/routes/lobby.js
+++ api/routes/lobby.js
@@ -7,6 +7,20 @@ import {createGamePassphrase, createGamePin} from "../utils/lobby";
 
 const router = express.Router();
 
+function validatePin(req, res, next) {
+    const {pin} = req.params;
+
+    // Check if 'pin' parameter follows the pin format we except - as in 6 digits long and only numerical characters
+    if (pin.length !== 6 || isNaN(pin)) {
+        return res.status(400).json({
+            status: false,
+            message: error.BAD_REQUEST,
+            extra: "Game PIN must be 6 digits."
+        });
+    } else {
+        next();
+    }
+}
 
 /**
  * @version api-v0.0.1
@@ -105,17 +119,68 @@ router.post("/", authenticate, async (req, res) => {
     }
 });
 
-router.delete("/:pin", authenticate, async (req, res) => {
+
+/**
+ * @version v1.0.0
+ * @method GET
+ * @url https://api.durachok.io/lobby/:id
+ * @example https://api.durachok.io/lobby/123456
+ *
+ * @description This route is used to check if the game exists, and if so will return
+ * the games metadata.
+ *
+ * @param {number} pin: the identifier number of the game.
+ *
+ * @error {BAD_REQUEST} if the pin isn't 6 digits long which is the standard.
+ * @error {BAD_REQUEST} if the pin isn't purely numerical as is the standard.
+ * @error {NOT_FOUND} if the id of the game doesn't exist in the records.
+ * @error {INTERNAL_SERVER_ERROR} if an attempt to retrieve the game fails.
+ *
+ * @return sends an OK response to requester with some game data.
+ *
+ * */
+router.get('/:pin', validatePin, authenticate, async (req, res) => {
     const {pin} = req.params;
 
-    // Check if 'pin' parameter follows the pin format we except - as in 6 digits long and only numerical characters
-    if (pin.length !== 6 || isNaN(pin)) {
-        return res.status(400).json({
+    const lobby = await Lobby.findOne({pin});
+
+    if (!lobby) {
+        return res.status(404).json({
             status: false,
-            message: error.BAD_REQUEST,
-            extra: "Game PIN must be 6 digits."
-        });
+            message: error.NON_EXISTENT_LOBBY,
+        })
     }
+
+    return res.status(200).json({
+        status: true,
+        item: lobby,
+    })
+});
+
+
+/**
+ * @version v1.0.0
+ * @method DELETE
+ * @url https://api.durachok.io/lobby/:id
+ * @example https://api.durachok.io/lobby/123456
+ *
+ * @description This route is used to delete the game with the given pin. If the pin is not the proper
+ * format, the standard BAD_REQUEST response is sent. If the requester is unauthorized to delete the
+ * current game, the server will respond with an UNAUTHORIZED response. If the request is valid, the
+ * object is deleted in the 'Games' mongo collection and the server responds with an OK.
+ *
+ * @param {number} pin: the identifier number of the game.
+ *
+ * @error {UNAUTHORIZED} if the requester doesn't have proper permissions.
+ * @error {BAD_REQUEST} if the pin isn't 6 digits long which is the standard.
+ * @error {BAD_REQUEST} if the pin isn't purely numerical as is the standard.
+ * @error {NOT_FOUND} if the id of the game doesn't exist in the records.
+ * @error {INTERNAL_SERVER_ERROR} if the current request can't be processed.
+ *
+ * @return sends an OK response to requester with some game data.
+ * */
+router.delete("/:pin", validatePin, authenticate, async (req, res) => {
+    const {pin} = req.params;
 
     // check that the requesting user is the owner/creator of the lobby
     const lobby = await Lobby.findOne({pin});
@@ -138,69 +203,55 @@ router.delete("/:pin", authenticate, async (req, res) => {
     }
 
     return await Lobby.deleteOne({pin}, (err) => {
-       if (err) {
-           return res.status(500).json({
-               status: false,
-               message: error.INTERNAL_SERVER_ERROR,
-               extra: "Couldn't delete the game at this time."
-           });
-       }
+        if (err) {
+            return res.status(500).json({
+                status: false,
+                message: error.INTERNAL_SERVER_ERROR,
+                extra: "Couldn't delete the game at this time."
+            });
+        }
 
-       return res.status(200).json({
-           status: true,
-           message: "Successfully delete game lobby."
-       })
+        return res.status(200).json({
+            status: true,
+            message: "Successfully delete game lobby."
+        })
     });
 });
 
 /**
  * @version v1.0.0
- * @method GET
- * @url https://api.durachok.io/lobby/:id
- * @example https://api.durachok.io/lobby/123456
+ * @method POST
+ * @url https://api.durachok.io/lobby/:id/join
+ * @example https://api.durachok.io/lobby/123456/join
  *
- * @description This route is used to check if the game exists, and if so will return
- * the games metadata.
+ * @description This route is used to join the given user to the game. It will use the requesters
+ * IP and a combination of generated ID, and a pin to create a JWT token, for authentication.
+ * The body of the request should also send over the passphrase to join the game. If the
+ * passphrase is incorrect, the server will respond with an unauthorized and prevent the player
+ * from joining the lobby. However, if the request is successful, the request will respond with
+ * a JWT token that should be used to authenticate any requests made to the game lobby.
  *
  * @param {number} pin: the identifier number of the game.
  *
+ * @error {UNAUTHORIZED} if the game passphrase is incorrect.
  * @error {BAD_REQUEST} if the pin isn't 6 digits long which is the standard.
  * @error {BAD_REQUEST} if the pin isn't purely numerical as is the standard.
  * @error {NOT_FOUND} if the id of the game doesn't exist in the records.
- * @error {INTERNAL_SERVER_ERROR} if an attempt to retrieve the game fails.
+ * @error {INTERNAL_SERVER_ERROR} if the current request can't be processed.
  *
  * @return sends an OK response to requester with some game data.
- *
  * */
-router.get('/:pin', authenticate, async (req, res, next) => {
+router.post("/:pin/join", validatePin, async (req, res) => {
     const {pin} = req.params;
 
-    // Check if 'pin' parameter follows the pin format we except - as in 6 digits long and only numerical characters
-    if (pin.length !== 6 || isNaN(pin)) {
-        return res.status(400).json({
-            status: false,
-            message: error.BAD_REQUEST,
-            extra: "Game PIN must be 6 digits."
-        });
-    }
 
-    const lobby = await Lobby.findOne({pin});
 
-    if (!lobby) {
-        return res.status(404).json({
-            status: false,
-            message: error.NON_EXISTENT_LOBBY,
-        })
-    }
+    const forwarded = req.headers['x-forwarded-for']
+    const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
 
-    return res.status(200).json({
-        status: true,
-        item: lobby,
-    })
-});
+    console.log(req.ip, ip);
 
-router.post("/:pin/join", authenticate, async (req, res, next) => {
-
+    return res.redirect(`/lobby/${pin}`);
 });
 
 export default router;
