763cb169e1b2de945c44de4d1667fd0fbc3b5259
Alex <alexander.fedotov.uk@gmail.com>
2020-12-09 20:49:48 +0000

    Setup basic interaction between client and server using socket.io

--- client/src/routes/Lobby/index.js
+++ client/src/routes/Lobby/index.js
@@ -6,25 +6,76 @@
  * @email <alexander.fedotov.uk@gmail.com>
  */
 
-import React, {useEffect} from "react";
-import {useHistory, useParams} from "react-router";
+import React from "react";
+import {io} from "socket.io-client";
+import {withRouter} from "react-router";
+import {getAuthTokens} from "../../utils/auth";
 
-const LobbyRoute = () => {
-    const {id} = useParams();
-    const history = useHistory();
 
-    // check that the user is authenticated for the current
-    // lobby
-    useEffect(() => {
-        console.log(id);
+class LobbyRoute extends React.Component {
+    constructor(props) {
+        super(props);
 
-    }, [id, history])
+        this.state = {id: null, ws: null};
+    }
 
-    return (
-        <>
-            Lobby
-        </>
-    );
-};
+    componentDidMount() {
+        const id =  this.props.match.params.id;
+        const socket = io(`localhost:5000/${id}`, {query: getAuthTokens(), transports: ["websocket"]});
 
-export default LobbyRoute;
+        // client-side
+        socket.on("connect", () => {
+            socket.emit("join_game", {});
+        });
+
+        socket.on("connect_error", err => {
+            console.log(err instanceof Error); // true
+            console.log(err.message); // not authorized
+            console.log(err.data); // { content: "Please retry later" }
+        });
+
+        socket.on("joined_game", (message) => {
+            console.log("players: ", message);
+        });
+
+        this.setState({
+            ws: socket,
+            id: id
+        })
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        const {id} = this.state;
+
+        return (
+            <div>
+                Lobby {id}
+            </div>
+        );
+    }
+
+
+}
+
+// const LobbyRoute = () => {
+//     const {id} = useParams();
+//     const history = useHistory();
+//
+//     // check that the user is authenticated for the current
+//     // lobby
+//     useEffect(() => {
+//         console.log(id);
+//
+//     }, [id, history])
+//
+//     return (
+//         <>
+//             Lobby
+//         </>
+//     );
+// };
+
+export default withRouter(LobbyRoute);
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -5,6 +5,20 @@
  * @parma {String} token - The new token to be added to the localstorage.
  * @parma {String} refreshToken - The new refresh token to be added to the localstorage.
  * */
+export function getAuthTokens() {
+    const token = localStorage.getItem("token");
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    return {token, refreshToken};
+}
+
+/**
+ * Function to update the localstorage with a new token and refresh
+ * token.
+ *
+ * @parma {String} token - The new token to be added to the localstorage.
+ * @parma {String} refreshToken - The new refresh token to be added to the localstorage.
+ * */
 export const updateTokens = (token, refreshToken) => {
     localStorage.setItem("token", token);
     localStorage.setItem("refreshToken", refreshToken);
