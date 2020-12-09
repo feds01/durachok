0f5e94574a4664f2a5f6cc40db0a7d95f5710232
Alex <alexander.fedotov.uk@gmail.com>
2020-12-09 20:49:48 +0000

    Setup basic interaction between client and server using socket.io

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // Import our environment variables
-import {Game} from "./api/common/game";
+import jwt from "jsonwebtoken";
 
 require('dotenv').config();
 
 import cors from 'cors';
 import logger from 'morgan';
-import * as http from 'http';
 import express from 'express';
 import mongoose from 'mongoose';
-import * as WebSocket from 'ws';
+import {Server} from "socket.io";
+import {createServer} from 'http';
+import * as error from "./api/error";
+import Lobby from './api/models/game';
+import Player from "./api/models/user";
+import {Game} from "./api/common/game";
 
 import userRouter from './api/routes/user';
 import lobbyRouter from './api/routes/lobby';
+import lobby from "./api/routes/lobby";
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -19,7 +24,7 @@ app.set('view engine', 'ejs');
 app.use(logger(process.env.NODE_ENV || 'dev'));
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 // Add our routes to the root router
 app.use('/api/user', userRouter);
@@ -45,26 +50,77 @@ app.use((err, req, res, next) => {
 });
 
 
-
 //initialize a simple http server
-const server = http.createServer(app);
+const server = createServer(app);
 
 //initialize the WebSocket server instance
-const wss = new WebSocket.Server({server});
+const io = new Server(server, {});
+const lobbies = io.of(/^\/\d{6}$/);
 
-wss.on('connection', (ws) => {
+/**
+ * Middleware function to check that the attempted lobby join exists.
+ * */
+lobbies.use(async (socket, next) => {
+    const lobbyPin = socket.nsp.name.split("/")[1];
 
+    // check that a game exists with the provided pin
+    const lobby = await Lobby.findOne({pin: lobbyPin});
+
+    if (!lobby) {
+        return next(new Error(error.NON_EXISTENT_LOBBY));
+    }
+
+    socket.lobby = lobby;
+    next();
+});
+
+lobbies.use((socket, next) => {
+
+    if (socket.handshake.query && socket.handshake.query.token) {
+        jwt.verify(socket.handshake.query.token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+            if (err) return next(new Error(error.AUTHENTICATION_FAILED));
+
+            // check that the nsp matched the pin or the user of the Durachok
+            // service is the owner of this lobby.
+            const isAdmin = typeof decoded.id !== "undefined";
+
+            if (isAdmin) {
+                const user = Player.findOne({_id: decoded.id});
+
+                // This shouldn't happen unless the user was deleted and the token is stale.
+                if (!user) {
+                    return next(new Error(error.AUTHENTICATION_FAILED));
+                }
+
+                // check that id of the owner is equal to the id in the JWT
+                if (user._id !== socket.lobby.owner._id) {
+                    return next(new Error(error.AUTHENTICATION_FAILED));
+                }
+            }
+
+            if (!isAdmin && socket.lobby.pin !== decoded.pin) {
+                return next(new Error(error.AUTHENTICATION_FAILED));
+            }
+
+            socket.isAdmin = isAdmin;
+            socket.decoded = decoded;
+            next();
+        });
+    } else {
+        next(new Error(error.AUTHENTICATION_FAILED));
+    }
+});
+
+lobbies.on('connect', (socket) => {
     //connection is up, let's add a simple simple event
-    ws.on('message', (message) => {
+    socket.on('join_game', (message) => {
 
         //log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        io.of(socket.nsp.name).emit("joined_game", {players: socket.lobby.players.map(p => p.name)});
     });
-
-    //send immediately a feedback to the incoming connection
-    ws.send('Hi there, I am a WebSocket server');
 });
+
 
 //start our server
 server.listen(process.env.PORT || 5000, () => {
