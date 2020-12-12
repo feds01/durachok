3b10b875a7176a017237a3020f81557614734b1c
alex <alexander.fedtov.uk@gmail.com>
2020-12-12 13:01:35 +0000

    Added loading screen to the lobby application

--- client/src/routes/Lobby/index.js
+++ client/src/routes/Lobby/index.js
@@ -7,20 +7,24 @@
  */
 
 import React from "react";
+import styles from "./index.module.scss";
 import {io} from "socket.io-client";
+import * as error from "../../error";
 import {withRouter} from "react-router";
+import Loader from "react-loader-spinner";
 import {getAuthTokens} from "../../utils/auth";
+import Divider from "@material-ui/core/Divider";
 
 
 class LobbyRoute extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {id: null, ws: null};
+        this.state = {id: null, ws: null, loaded: false, lobby: {}, error: null};
     }
 
     componentDidMount() {
-        const id =  this.props.match.params.id;
+        const id = this.props.match.params.id;
         const socket = io(`localhost:5000/${id}`, {query: getAuthTokens(), transports: ["websocket"]});
 
         // client-side
@@ -29,32 +33,89 @@ class LobbyRoute extends React.Component {
         });
 
         socket.on("connect_error", err => {
+
+            // ensure that the transmitted 'connection_error' is an
+            // error object from the server, then use the connection
+            // message string to determine the next action
+            if (err instanceof Error) {
+
+                // if the user is not allowed to join this lobby: re-direct the
+                // user back to the home page from where they can re-try to
+                // join the lobby
+                if (err.message === error.AUTHENTICATION_FAILED || err.message === error.NON_EXISTENT_LOBBY) {
+                    this.props.history.push("/");
+                }
+
+                // if the user is authenticated but they are trying to join a
+                // lobby that already has the maximum players or they are currently
+                // in a round, the server will return a error.LOBBY_FULL
+                if (err.message === error.LOBBY_FULL) {
+                    this.setState({error: error.LOBBY_FULL});
+                }
+            }
+
             console.log(err instanceof Error); // true
             console.log(err.message); // not authorized
             console.log(err.data); // { content: "Please retry later" }
         });
 
+
+        // if the client is successfully authenticated and joined the lobby
+        // on the server, then we can begin to load the lobby...
         socket.on("joined_game", (message) => {
-            console.log("players: ", message);
+            // console.log("players: ", message);
+            this.setState({
+                loaded: true,
+                lobby: message
+            });
         });
 
         this.setState({
             ws: socket,
             id: id
-        })
+        });
     }
 
     componentWillUnmount() {
     }
 
     render() {
-        const {id} = this.state;
+        const {id, loaded, lobby, ws} = this.state;
 
-        return (
-            <div>
-                Lobby {id}
-            </div>
-        );
+
+        console.log(lobby.players)
+        if (!loaded) {
+            return (
+                <div
+                    style={{
+                        display: "flex",
+                        height: "100%",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}
+                >
+                    <div>
+                        <Loader className={styles.lobbyLoader} type="Bars" color="#FFFFFF" height={80} width={80}/>
+                        <b>Joining Lobby...</b>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <h1 className={styles.lobbyTitle}>Lobby {id}</h1>
+                    <Divider style={{backgroundColor: "rgba(172, 170, 190, 1)"}}/>
+                    <div className={styles.lobbyPlayers}>
+                        {
+                            lobby.players.map((player, index) => (
+                                <span key={index}>{player}</span>
+                            ))
+                        }
+                    </div>
+                </div>
+            );
+        }
     }
 
 
