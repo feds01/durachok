581973c3ed3e7e347488a9a46616a21049b883c1
alex <alexander.fedotov.uk@gmail.com>
2021-02-01 17:04:49 +0000

    serve player statistics to clients.

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, {Document, Schema} from 'mongoose';
+import {UserStatisticsType} from "../common/statistics";
 
 export interface IUser extends Document {
     email: string,
@@ -6,6 +7,7 @@ export interface IUser extends Document {
     password: string,
     image: boolean,
     createdAt: number,
+    statistics?: UserStatisticsType,
 }
 
 
@@ -14,6 +16,7 @@ const UserSchema = new Schema({
     email: { type: String, required: true, unique: true},
     name: {type: String, required: false, unique: true},
     password: {type: String, required: true, minLength: 12},
+    statistics: {type: Object, required: false},
     createdAt: {type: Date, required: false, default: Date.now},
 });
 
