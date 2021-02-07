657ba26c6b97d5e87ee047f271caadfd54a6a6b8
alex <alexander.fedotov.uk@gmail.com>
2021-02-07 17:46:46 +0000

    remove nocheckin

--- src/socket/handlers/startGame.ts
+++ src/socket/handlers/startGame.ts
@@ -64,7 +64,6 @@ async function handler(context: any, socket: Socket, io?: Server | null) {
         shortGameDeck,
         freeForAll,
     });
-    game.deck = [];
 
     // save the game into mongo
     await Lobby.updateOne({_id: socket.lobby._id}, {game: game.serialize(), createdAt: Date.now()});
