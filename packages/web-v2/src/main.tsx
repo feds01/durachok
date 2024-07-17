03dd9b1118d5bff13dbef07144a6dbfddeb974f8
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-17 11:37:51 -0500

    packages(web-v2): add tanstack router

---
+++ packages/web-v2/src/main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { Navigate, RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({
+    routeTree,
+    defaultNotFoundComponent: () => {
+        return <Navigate to="/" />;
+    },
+});
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
+);
