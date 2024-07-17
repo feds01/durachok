ffaafb82414c6539ff7b1333130c468e3309afa0
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-17 15:20:30 -0500

    packages(web-v2): added assets and SVG loading capabilities

--- packages/web-v2/vite.config.ts
+++ packages/web-v2/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [svgr(), TanStackRouterVite(), react()],
 })
