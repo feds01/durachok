fb24fe6c0fd2865b713add909aeab5b0d4087cca
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-18 11:00:25 -0500

    chore: add prettier to `engine`

---
+++ packages/engine/.prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    "trailingComma": "all",
+    "quoteProps": "preserve",
+    "importOrderSeparation": true,
+    "importOrderSortSpecifiers": true,
+    "importOrder": ["^components/(.*)$", "^[./]"],
+    "plugins": [require.resolve("@trivago/prettier-plugin-sort-imports")],
+};
