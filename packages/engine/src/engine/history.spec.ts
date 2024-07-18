b97473c64d7cecced06c6ef84be9d0217bdcfb74
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-18 11:00:12 -0500

    packages(engine): use `vitest` instead of `jest`

--- packages/engine/src/engine/history.spec.ts
+++ packages/engine/src/engine/history.spec.ts
@@ -1,23 +1,26 @@
-import {Game} from "./game";
-import {History, HistoryNode} from "./history";
+import { describe, expect, test } from "vitest";
 
+import { Game } from "./game";
+import { History, HistoryNode } from "./history";
 
 describe("History sub-system tests", () => {
-
     describe("History", () => {
         test("should create a new game without crashing", () => {
             const game = new Game(["player1", "player2"], null);
 
-            expect(() => new History(game.serialize().state, null)).not.toThrow()
+            expect(
+                () => new History(game.serialize().state, null),
+            ).not.toThrow();
         });
     });
 
-
-    describe("HistoryNode",  () => {
-        test(("Adding an action to a finalised HistoryNode should throw"), () => {
+    describe("HistoryNode", () => {
+        test("Adding an action to a finalised HistoryNode should throw", () => {
             const node = new HistoryNode([], true);
 
-            expect(() => node.addAction({type: "forfeit", player: "player"})).toThrow();
+            expect(() =>
+                node.addAction({ type: "forfeit", player: "player" }),
+            ).toThrow();
         });
     });
 });
