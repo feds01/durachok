a0601299b2392db128cedcbd9b9afecef1d6d847
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-18 11:00:12 -0500

    packages(engine): use `vitest` instead of `jest`

--- packages/engine/src/engine/game.spec.ts
+++ packages/engine/src/engine/game.spec.ts
@@ -1,23 +1,27 @@
-import {defaultSettings, Game} from "./game";
+import { describe, expect, test } from "vitest";
 
+import { Game, defaultSettings } from "./game";
 
 describe("Game tests", () => {
     test("should create a new game without crashing", () =>
-        expect(() => new Game(["player1", "player2"], null)).not.toThrow()
-    )
+        expect(() => new Game(["player1", "player2"], null)).not.toThrow());
 
     test("Game constructor should throw when shortDeck is enabled and there are more than six players", () =>
-        expect(() => new Game(["0", "1", "2", "3", "4", "5", "6"], null, {
-            ...defaultSettings,
-            shortGameDeck: true,
-        })).toThrow()
-    )
+        expect(
+            () =>
+                new Game(["0", "1", "2", "3", "4", "5", "6"], null, {
+                    ...defaultSettings,
+                    shortGameDeck: true,
+                }),
+        ).toThrow());
 
     test("Game.fromState should produce the same object", () => {
         const game = new Game(["player1", "player2"], null, defaultSettings);
         const save = game.serialize();
 
-        expect(Game.fromState(save.state, save.history, defaultSettings)).toStrictEqual(game);
+        expect(
+            Game.fromState(save.state, save.history, defaultSettings),
+        ).toStrictEqual(game);
     });
 
     test("Game.fromState re-creates correct state after move", () => {
@@ -28,6 +32,8 @@ describe("Game tests", () => {
         game.addCardToTableTop(attacker, game.getPlayer(attacker).deck[0]);
 
         const save = game.serialize();
-        expect(Game.fromState(save.state, save.history, defaultSettings)).toStrictEqual(game);
+        expect(
+            Game.fromState(save.state, save.history, defaultSettings),
+        ).toStrictEqual(game);
     });
 });
