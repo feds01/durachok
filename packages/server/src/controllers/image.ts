e0169d5bb3e4c6b5f85d8bc1294f2548d78fcf7d
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-19 13:48:20 -0500

    packages(server): add `ImageService` utility service

---
+++ packages/server/src/controllers/image.ts
@@ -0,0 +1,12 @@
+import { Logger } from "winston";
+
+export class ImageService {
+    public constructor(
+        private readonly logger: Logger,
+    ) {}
+
+    /** Get an image associated with the user, i.e. their avatar. */
+    public async getUserImage(userId: string): Promise<string | undefined> {
+        return;
+    }
+}
