08f7f84527df6efd38cb1cb66bbb2d297936fd25
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-19 17:17:21 -0500

    packages(web-v2): add redirect behaviour on an-authenticated access to `/user`

--- packages/web-v2/src/routes/login.tsx
+++ packages/web-v2/src/routes/login.tsx
@@ -1,6 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+const LoginSearchSchema = z.object({
+    redirect: z.string().optional(),
+});
 
 export const Route = createFileRoute("/login")({
+    validateSearch: (search) => LoginSearchSchema.parse(search),
+    beforeLoad: async ({ context, search }) => {
+        if (context.auth.kind === "logged-in") {
+            throw redirect({
+                to: search.redirect ?? "/user",
+            });
+        }
+    },
     component: Login,
 });
 
