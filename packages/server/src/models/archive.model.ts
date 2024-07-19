8996fa4b9e4a9a5ea24f510bc2689876ad78d0d4
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-19 13:46:44 -0500

    packages(server): cleanup models

---
+++ packages/server/src/models/archive.model.ts
@@ -0,0 +1,56 @@
+import { IUser } from "./user.model";
+import mongoose, { Document, Schema } from 'mongoose';
+import { GameState, HistoryState } from "shared";
+
+export interface IArchivedGame extends Document {
+    maxPlayers: number,
+    randomPlayerOrder: boolean,
+    owner: IUser['_id'],
+    game: {
+        history: HistoryState,
+        state: GameState,
+    },
+}
+
+const ArchivedGameSchema = new Schema<IArchivedGame>({
+    // game preferences need to be preserved
+    maxPlayers: { type: Number, required: true },
+    randomPlayerOrder: { type: Boolean, required: true },
+
+    // owner details need to be preserved
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+
+    // the actual game needs to be preserved
+    game: {
+        type: {
+            history: { type: Object, required: true },
+            state: {
+                type: {
+                    players: {
+                        type: Map,
+                        of: {
+                            deck: { type: [String] },
+                            canAttack: { type: Boolean },
+                            beganRound: { type: Boolean },
+                            turned: { type: Boolean },
+                            isDefending: { type: Boolean }
+                        }
+                    },
+                    deck: { type: [String] },
+                    victory: { type: Boolean },
+                    trumpCard: {
+                        type: {
+                            value: { type: Number },
+                            suit: { type: String },
+                            card: { type: String },
+                        }
+                    },
+                    tableTop: { type: Map, of: String }
+                }
+            }
+        },
+        required: true,
+    }
+});
+
+export default mongoose.model<IArchivedGame>('archive', ArchivedGameSchema);
