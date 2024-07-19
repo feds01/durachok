7d0ee766fd2d87294c703622e91094f017afc833
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-19 17:15:38 -0500

    packages(web-v2): move `Logo` to its own component

---
+++ packages/web-v2/src/components/Logo.tsx
@@ -0,0 +1,32 @@
+import { css } from "@emotion/css";
+
+import PlayingCards from "../assets/image/playing-cards.svg";
+
+const Logo = ({ size }: { size: number }) => (
+    <div>
+        <h2
+            className={css`
+                font-family: "Playfair Display", serif !important;
+                margin: 0;
+                font-weight: 900;
+                font-style: italic;
+                color: #dad8ec;
+
+                @media (max-width: 600px) {
+                    font-size: 40px !important;
+                }
+            `}
+            style={{ fontSize: size }}
+        >
+            <img
+                src={PlayingCards}
+                width={size * 0.75}
+                height={size * 0.75}
+                alt={""}
+            />
+            Durachok
+        </h2>
+    </div>
+);
+
+export default Logo;
--- packages/web-v2/src/routes/index.tsx
+++ packages/web-v2/src/routes/index.tsx
@@ -3,39 +3,12 @@ import { Link, createFileRoute } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 
 import PlayingCard from "../assets/image/playing-card.svg?react";
-import PlayingCards from "../assets/image/playing-cards.svg";
+import Logo from "../components/Logo";
 import GamePrompt from "../forms/GamePrompt";
 
 export const Route = createFileRoute("/")({
     component: Index,
 });
-
-const Logo = ({ size }: { size: number }) => (
-    <div>
-        <h2
-            className={css`
-                font-family: "Playfair Display", serif !important;
-                margin: 0;
-                font-weight: 900;
-                font-style: italic;
-                color: #dad8ec;
-
-                @media (max-width: 600px) {
-                    font-size: 40px !important;
-                }
-            `}
-            style={{ fontSize: size }}
-        >
-            <img
-                src={PlayingCards}
-                width={size * 0.75}
-                height={size * 0.75}
-                alt={""}
-            />
-            Durachok
-        </h2>
-    </div>
-);
 
 const random = (max: number) => Math.floor(Math.random() * max);
 
