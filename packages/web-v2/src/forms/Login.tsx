6d374126337efe20af70450f87f5826ae921e18e
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-19 17:17:38 -0500

    packages(web-v2): implement `/login` page

---
+++ packages/web-v2/src/forms/Login.tsx
@@ -0,0 +1,103 @@
+import { css } from "@emotion/css";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import ControlledTextField from "../components/ControlledTextField";
+import SubmitButton from "../components/SubmitButton";
+import { expr } from "../utils";
+import { trpc } from "../utils/trpc";
+import { UserEmailSchema } from "../valdiators/user";
+
+const LoginSchema = z.object({
+    credential: z.string(),
+    password: z.string().min(8),
+});
+
+export type Login = z.infer<typeof LoginSchema>;
+export type LoginResult = {
+    name: string;
+    email: string;
+    token: string;
+    refreshToken: string;
+};
+
+type Props = {
+    onSuccess: (result: LoginResult) => Promise<void>;
+};
+
+export default function Login({ onSuccess }: Props) {
+    const loginWith = trpc.auth.login.useMutation();
+    const form = useForm<Login>({
+        resolver: zodResolver(LoginSchema),
+        reValidateMode: "onChange",
+        mode: "onChange",
+        defaultValues: {
+            credential: "",
+            password: "",
+        },
+    });
+
+    const onSubmit = async (login: Login) => {
+        try {
+            const credentials = expr(() => {
+                const maybeEmail = UserEmailSchema.safeParse(login.credential);
+                if (maybeEmail.success) {
+                    return { email: login.credential };
+                } else {
+                    return { name: login.credential };
+                }
+            });
+
+            const result = await loginWith.mutateAsync({
+                ...credentials,
+                password: login.password,
+            });
+            await onSuccess(result);
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                form.setError("password", {
+                    type: "manual",
+                    message: e.message,
+                });
+            }
+        }
+    };
+
+    return (
+        <form
+            className={css`
+                flex-grow: 1;
+                display: flex;
+                flex-direction: column;
+            `}
+            onSubmit={form.handleSubmit(onSubmit)}
+        >
+            <ControlledTextField
+                name="credential"
+                control={form.control}
+                textFieldProps={{
+                    placeholder: "Username or Email",
+                    autoFocus: true,
+                    autoComplete: "off",
+                }}
+            />
+            <ControlledTextField
+                name="password"
+                control={form.control}
+                textFieldProps={{
+                    placeholder: "Password",
+                    autoComplete: "off",
+                    sx: {
+                        pt: "2em",
+                    },
+                }}
+            />
+            <SubmitButton
+                label="Login"
+                type="submit"
+                isSubmitting={form.formState.isSubmitting}
+            />
+        </form>
+    );
+}
--- packages/web-v2/src/routes/login.tsx
+++ packages/web-v2/src/routes/login.tsx
@@ -1,5 +1,13 @@
-import { z } from "zod";
+import { css } from "@emotion/css";
 import { createFileRoute, redirect } from "@tanstack/react-router";
+import { Link, useNavigate } from "@tanstack/react-router";
+import { motion } from "framer-motion";
+import { z } from "zod";
+
+import Logo from "../components/Logo";
+import { useAuthDispatch } from "../contexts/auth";
+import LoginForm, { LoginResult } from "../forms/Login";
+
 const LoginSearchSchema = z.object({
     redirect: z.string().optional(),
 });
@@ -16,6 +24,94 @@ export const Route = createFileRoute("/login")({
     component: Login,
 });
 
+const container = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    align-items: center;
+
+    @media (max-width: 500px) {
+        justify-content: flex-start;
+
+        & > div {
+            display: flex;
+            flex-direction: column;
+            flex-grow: 1;
+            overflow: hidden;
+        }
+    }
+`;
+
+const login = css`
+    background: #1a1d3d;
+    box-shadow:
+        0 10px 20px rgba(0, 0, 0, 0.19),
+        0 6px 6px rgba(0, 0, 0, 0.23);
+    padding: 5em 2em;
+    border-radius: 12px;
+
+    width: 480px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin: 0 auto;
+
+    & input {
+        background: #3b3d54;
+    }
+
+    & h2 {
+        font-size: 28px;
+        font-family: "Cabin", sans-serif;
+    }
+
+    @media (max-width: 500px) {
+        flex-grow: 1;
+        width: 100% !important;
+        display: block !important;
+        border-radius: 0 !important;
+        box-shadow: none !important;
+    }
+`;
+
 export default function Login() {
-    return <div>Login</div>;
+    const { redirect } = Route.useSearch();
+    const navigator = useNavigate();
+    const auth = useAuthDispatch();
+
+    const onSuccess = async (result: LoginResult) => {
+        auth({ type: "LOGIN", payload: result });
+        navigator({ to: redirect ?? "/user" });
+    };
+
+    return (
+        <div className={container}>
+            <motion.div
+                transition={{ duration: 0.5 }}
+                initial={{ x: "calc(-100vw)" }}
+                animate={{ x: 0 }}
+                exit={{ x: "100vw" }}
+            >
+                <div className={login}>
+                    <Logo size={48} />
+                    <h2>Login</h2>
+                    <LoginForm onSuccess={onSuccess} />
+                    <p
+                        className={css`
+                            color: white;
+                            font-size: 15px;
+
+                            & a {
+                                color: dodgerblue;
+                            }
+                        `}
+                    >
+                        New User? Create account{" "}
+                        <Link to={"/register"}>here</Link>
+                    </p>
+                </div>
+            </motion.div>
+        </div>
+    );
 }
