a6e1fefc319ae73c765d8b244d420052fe9cbf09
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-21 18:22:30 -0400

    packages(server): add route `duration`, `path`, and `type` logging

--- packages/server/src/controllers/image.ts
+++ packages/server/src/controllers/image.ts
@@ -3,6 +3,12 @@ import { Logger } from "winston";
 export class ImageService {
     public constructor(private readonly logger: Logger) {}
 
+    /** Update a user image, given the buffer. */
+    public async updateUserImage(
+        userId: string,
+        buffer: Buffer,
+    ): Promise<void> {}
+
     /** Get an image associated with the user, i.e. their avatar. */
     public async getUserImage(userId: string): Promise<string | undefined> {
         return;
