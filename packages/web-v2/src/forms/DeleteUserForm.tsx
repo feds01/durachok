5eed992c19b49bd7e76ddd74900dd50d22c75b4e
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-21 15:44:34 -0400

    packages(web-v2): add skeleton to user settings page

---
+++ packages/web-v2/src/forms/DeleteUserForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// type Props = {}
+
+const DeleteUserForm = (/* props: Props */) => {
+    return <div>DeleteUserForm</div>;
+};
+
+export default DeleteUserForm;
---
+++ packages/web-v2/src/forms/UpdateUserForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// type Props = {}
+
+const UpdateUserForm = (/* props: Props */) => {
+    return <div>UpdateUserForm</div>;
+};
+
+export default UpdateUserForm;
