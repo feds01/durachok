38655306edf87049420d20a06aa8464802c4dc9c
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-22 18:34:48 -0400

    apps(server): use `ImageRepo` as abstraction layer for image storage access

--- apps/server/src/controllers/image.ts
+++ apps/server/src/controllers/image.ts
@@ -1,32 +1,14 @@
-import AWS, { CloudFront, S3 } from "aws-sdk";
 import { Logger } from "winston";
 
-import {
-    AWS_ACCESS_KEY,
-    AWS_BUCKET_NAME,
-    AWS_REGION,
-    AWS_SECRET_ACCESS_KEY,
-} from "../config";
-import { expr } from "../utils";
-
-// Update aws sdk configuration
-AWS.config.update({
-    accessKeyId: AWS_ACCESS_KEY, // Access key ID
-    secretAccessKey: AWS_SECRET_ACCESS_KEY, // Secret access key
-    region: AWS_REGION,
-});
+import { ImageRepo } from "../repos/image";
 
 const MAX_FILE_SIZE = 1048576; // 1 megabyte
 
-// Initiate the S3 API
-const s3 = new AWS.S3({
-    region: process.env.AWS_REGION,
-    apiVersion: "2006-03-01",
-});
-const cloudfront = new AWS.CloudFront();
-
 export class ImageService {
-    public constructor(private readonly logger: Logger) {}
+    public constructor(
+        private readonly logger: Logger,
+        private readonly imageRepo: ImageRepo,
+    ) {}
 
     /**
      * This function will simply convert a raw image which is base64 format into
@@ -74,64 +56,16 @@ export class ImageService {
         const decodedImage = this.decodeImage(image);
         const filename = this.getUserImageKey(userId);
 
-        const putRequest: S3.Types.PutObjectRequest = {
-            Bucket: AWS_BUCKET_NAME,
-            Key: filename,
-            Body: decodedImage,
-            ContentType: "image/jpeg",
-        };
-        const invalidationRequest: CloudFront.Types.CreateInvalidationRequest =
-            {
-                DistributionId: process.env.AWS_CLOUDFRONT_ID!,
-                InvalidationBatch: {
-                    CallerReference: Date.now().toString(),
-                    Paths: {
-                        Quantity: 1,
-                        Items: [filename],
-                    },
-                },
-            };
-
-        try {
-            await Promise.all([
-                s3.putObject(putRequest).promise(),
-                cloudfront.createInvalidation(invalidationRequest).promise(),
-            ]);
-        } catch (e: unknown) {
-            this.logger.warn(`Error updating user image: ${e}`);
-            throw new Error("Failed to update user image.");
-        }
+        await this.imageRepo.saveImage(filename, decodedImage);
     }
 
     /** Get an image associated with the user, i.e. their avatar. */
     public async getUserImage(userId: string): Promise<string | undefined> {
-        const params: S3.Types.HeadObjectRequest = {
-            Bucket: AWS_BUCKET_NAME,
-            Key: this.getUserImageKey(userId),
-        };
-
-        return await expr(async () => {
-            try {
-                await s3.headObject(params).promise();
-                return s3.getSignedUrl("getObject", params);
-            } catch (e) {
-                this.logger.warn(`Error fetching user image: ${e}`);
-                return;
-            }
-        });
+        return this.imageRepo.getImage(this.getUserImageKey(userId));
     }
 
     /** Delete the image associated with the user. */
     public async deleteUserImage(userId: string): Promise<void> {
-        const params: S3.Types.DeleteObjectRequest = {
-            Bucket: AWS_BUCKET_NAME,
-            Key: this.getUserImageKey(userId),
-        };
-
-        try {
-            await s3.deleteObject(params).promise();
-        } catch (e) {
-            this.logger.warn(`Error deleting user image: ${e}`);
-        }
+        await this.imageRepo.deleteImage(this.getUserImageKey(userId));
     }
 }
