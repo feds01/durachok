c9568f37661c2d79cb72eb514a05c311f449876c
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-21 23:26:59 -0400

    chore: configure top-level prettier and add `turbo` build system

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+    arrowParens: "always",
+    printWidth: 120,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    semi: true,
+    trailingComma: "all",
+    tabWidth: 4,
+};
+
+module.exports = config;
