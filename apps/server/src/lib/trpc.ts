0db4c75e57467794ce47406729ab094916beeee0
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-22 22:23:33 -0400

    apps: add custom serialisation/de-serialisation for `Buffer` types

--- apps/server/src/lib/trpc.ts
+++ apps/server/src/lib/trpc.ts
@@ -66,6 +66,20 @@ export const createContext = async ({
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
 
+/** 
+ * Register a hook to convert `Buffer`s to base64
+ * 
+ * @@Todo: maybe move this to `packages/transport`?
+ */
+superjson.registerCustom<Buffer, string>(
+    {
+        isApplicable: (value): value is Buffer => Buffer.isBuffer(value),
+        serialize: (value) => value.toString("base64"),
+        deserialize: (value) => Buffer.from(value, "base64"),
+    },
+    "buffer",
+);
+
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
