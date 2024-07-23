74fc51730565ed79ec8023de51c888fabe45c661
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-22 23:23:23 -0400

    app(web): move authentication types/schemas to `types/auth` and add registration types

---
+++ apps/web/src/types/auth.ts
@@ -0,0 +1,54 @@
+import { z } from "zod";
+
+import {
+    UserEmailSchema,
+    UserNameSchema,
+    UserPasswordSchema,
+} from "../valdiators/user";
+
+export const LoginFormSchema = z.object({
+    /** Either a user email or user name */
+    credential: z.string(),
+    /** The user's password */
+    password: UserPasswordSchema,
+});
+
+export type LoginFormData = z.infer<typeof LoginFormSchema>;
+
+export const RegisterFormSchema = z
+    .object({
+        /** The user's name */
+        name: UserNameSchema,
+        /** The user's email */
+        email: UserEmailSchema,
+        /** The user's password */
+        password: UserPasswordSchema,
+        /** The user's password confirmation */
+        passwordConfirmation: UserPasswordSchema,
+        /** The Google ReCAPTCHA token */
+        reCaptchaToken: z.string(),
+    })
+    .superRefine((data, ctx) => {
+        // Check if the passwords match.
+        if (data.password !== data.passwordConfirmation) {
+            ctx.addIssue({
+                code: "custom",
+                message: "Passwords do not match",
+                path: ["passwordConfirmation"],
+            });
+            return false;
+        }
+        return true;
+    });
+
+export type RegisterFormData = z.infer<typeof RegisterFormSchema>;
+
+/** The result of the data that is returned from registering or logging in. */
+export type AuthResult = {
+    id: string;
+    name: string;
+    email: string;
+    token: string;
+    image?: string;
+    refreshToken: string;
+};
