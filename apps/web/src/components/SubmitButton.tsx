32b3f210afc8e186b5402bce252fd7ec8a05760b
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-07-25 00:22:36 -0400

    web: simplify `SubmitButton` component

--- apps/web/src/components/SubmitButton.tsx
+++ apps/web/src/components/SubmitButton.tsx
@@ -1,41 +1,27 @@
-import Button from "@mui/material/Button";
+import Button, { ButtonProps } from "@mui/material/Button";
 import { ThreeDots } from "react-loader-spinner";
 
-import { isDef } from "../utils";
-
-type Props = {
+interface Props extends ButtonProps {
     /** Whether the button is in a submitting state. */
     isSubmitting: boolean;
-    /** Whether the button is disabled. */
-    disabled?: boolean;
-    /** The function to call when the button is clicked. */
-    onClick?: () => void;
     /** The text to display on the button. */
     label?: string;
-    /** The kind of button. */
-    type?: "button" | "submit" | "reset";
-    /** Optional class name string. */
-    className?: string;
-};
+}
 
 export default function SubmitButton({
     disabled,
-    type,
     label,
     isSubmitting,
-    className,
-    onClick,
+    ...rest
 }: Props) {
     return (
         <Button
             variant={"contained"}
-            {...(isDef(className) && { className })}
             disableElevation
             disableRipple
-            type={type ?? "button"}
-            {...(isDef(onClick) && { onClick })}
             disabled={disabled || isSubmitting}
             color={"primary"}
+            {...rest}
         >
             {isSubmitting ? (
                 <ThreeDots color="#FFFFFF" height={20} width={40} />
