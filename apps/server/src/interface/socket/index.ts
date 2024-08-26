c83e77657dde58a6b4498d1df56c22574d9c8f78
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-08-26 23:06:14 +0100

    server(socket): pass in `Context` into socket actions

--- apps/server/src/interface/socket/index.ts
+++ apps/server/src/interface/socket/index.ts
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import { attachSockets } from "zod-sockets";
 
 import { APP_URL } from "../../config";
+import { Context, createContext } from "../../lib/context";
 import logger from "../../lib/logger";
 import { actions } from "./actions";
 import { config as socketConfig } from "./config";
@@ -20,15 +21,31 @@ const io = new Server({
  */
 export async function connectSocket(server: HTTPServer) {
     logger.info("Attempting to connect socket server to HTTP server");
+
+    // Create the context, and prepare it for being passed into
+    // the sockets.
+    const loggerWithCtx = logger as Logger & {
+        ctx: Context;
+    };
+    loggerWithCtx.ctx = createContext(logger);
+
     attachSockets({
         io,
         target: server,
         config: socketConfig,
         actions,
-        logger,
+        logger: loggerWithCtx,
     });
 }
 
 declare module "zod-sockets" {
-    interface LoggerOverrides extends Logger {}
+    interface LoggerOverrides extends Logger {
+        /**
+         * The context of the current server execution, this is effectively a
+         * a dependency injection.
+         *
+         * @@Hack: this is a massive hack! Discussion: https://github.com/RobinTail/zod-sockets/discussions/272
+         */
+        ctx: Context;
+    }
 }
