44640eed4416935d54b2ba3297cc4c7aabe2be64
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-08-26 18:52:07 +0100

    engine: remove old exports from `index`

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,6 +1,4 @@
 export { shuffleArray, getRandomKey } from "./utils";
 export { Game } from "./engine/game";
-export { GameState } from "./engine/state";
-export { Card } from "./engine/card";
 export { CardNumerics, CardSuits, TableSize } from "./engine/consts";
-export { HistoryState, Action, HistoryNode, History } from "./engine/history";
+export { HistoryState, HistoryNode, History } from "./engine/history";
