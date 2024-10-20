2c7cfb9acc5f29e8dadca37c2d2693a43cc4a097
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-10-20 22:28:42 +0100

    web: support relative imports via `@`

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), TanStackRouterVite(), react()],
+  plugins: [svgr(), TanStackRouterVite(), tsconfigPaths(), react()],
 })
